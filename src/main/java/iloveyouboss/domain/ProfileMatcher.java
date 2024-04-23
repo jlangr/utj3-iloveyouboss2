13cae33e6fa1d7982d1d869004dd3954a714fb66
Jeff L <jeff@langrsoft.com>
2024-04-23 09:29:54 -0600

    highlights

--- src/main/java/iloveyouboss/domain/ProfileMatcher.java
+++ src/main/java/iloveyouboss/domain/ProfileMatcher.java
@@ -16,15 +16,21 @@ public class ProfileMatcher {
    // START:impl
    public Map<Profile, Integer> scoreProfiles(Criteria criteria)
       throws ExecutionException, InterruptedException {
+      // START_HIGHLIGHT
       var futures = new ArrayList<Future<Map<Profile, Integer>>>();
+      // END_HIGHLIGHT
       for (var profile : profiles)
          futures.add(executorService.submit(() ->
+            // START_HIGHLIGHT
             Map.of(profile,
                    profile.matches(criteria) ? profile.score(criteria) : 0)));
+      // END_HIGHLIGHT
 
+      // START_HIGHLIGHT
       var finalScores = new HashMap<Profile, Integer>();
       for (var future: futures)
          finalScores.putAll(future.get());
+      // END_HIGHLIGHT
 
       executorService.shutdown();
       return finalScores;
