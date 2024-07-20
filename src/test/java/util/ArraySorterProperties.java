156609a12cfce74dd3e3245d876b7ef827bf26c8
Jeff L <jeff@langrsoft.com>
2024-07-20 16:22:46 -0600

    proper return

--- src/test/java/util/ArraySorterProperties.java
+++ src/test/java/util/ArraySorterProperties.java
@@ -3,41 +3,41 @@ package util;
 // START:test
 import static java.util.Arrays.fill;
 import static java.util.Arrays.sort;
-import static org.junit.jupiter.api.Assertions.*;
 import net.jqwik.api.*;
+import java.util.Arrays;
 
 public class ArraySorterProperties {
    ArraySorter arraySorter = new ArraySorter();
 
    @Property
-   void returnsSameArrayWhenAlreadySorted(@ForAll int[] array) {
+   boolean returnsSameArrayWhenAlreadySorted(@ForAll int[] array) {
       sort(array);
       var expected = array.clone();
 
       arraySorter.inPlaceInsertionSort(array);
 
-      assertArrayEquals(expected, array);
+      return Arrays.equals(expected, array);
    }
 
    @Property
-   void returnsSameArrayWhenAllSameElements(@ForAll int element) {
+   boolean returnsSameArrayWhenAllSameElements(@ForAll int element) {
       var array = new int[12];
       fill(array, element);
-      int[] expected = array.clone();
+      var expected = array.clone();
 
       arraySorter.inPlaceInsertionSort(array);
 
-      assertArrayEquals(expected, array);
+      return Arrays.equals(expected, array);
    }
 
    @Property
-   void sortsAscendingWhenRandomUnsortedArray(@ForAll int[] array) {
+   boolean sortsAscendingWhenRandomUnsortedArray(@ForAll int[] array) {
       var expected = array.clone();
       sort(expected);
 
       arraySorter.inPlaceInsertionSort(array);
 
-      assertArrayEquals(expected, array);
+      return Arrays.equals(expected, array);
    }
 }
 // END:test
